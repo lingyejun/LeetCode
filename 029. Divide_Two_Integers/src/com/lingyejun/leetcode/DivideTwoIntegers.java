ae0be1c6abc4f684d176421e6b70e943c7ee4412
lilingye <lilingyejun@gmail.com>
2018-11-02 23:52:47 +0800

    Update 029. Divide_Two_Integers/src/com/lingyejun/leetcode/DivideTwoIntegers.java

---
+++ 029. Divide_Two_Integers/src/com/lingyejun/leetcode/DivideTwoIntegers.java
@@ -0,0 +1,31 @@
+package com.lingyejun.leetcode;
+
+/**
+ * 29. Divide Two Integers
+ *
+ * Given two integers dividend and divisor, divide two integers without using multiplication, division and mod operator.
+ * Return the quotient after dividing dividend by divisor.
+ * The integer division should truncate toward zero.
+ *
+ * Example 1:
+ * Input: dividend = 10, divisor = 3
+ * Output: 3
+ *
+ * @Author: LeeChao
+ * @Date: 2018/11/2
+ * @Describe:
+ * @Modified By:
+ */
+public class DivideTwoIntegers {
+
+    public int divide(int dividend, int divisor) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int dividend = 10;
+        int divisor = 3;
+        DivideTwoIntegers dti = new DivideTwoIntegers();
+        int a = dti.divide(dividend,divisor);
+    }
+}
