3e3fdf95a6682e5f9314a47d9ce33a0bda2c6c65
lilingye <lilingyejun@gmail.com>
2018-12-06 23:02:41 +0800

    Update ImplementStrStr.java

--- 028. Implement_strStr/src/com/lingyejun/leetcode/ImplementStrStr.java
+++ 028. Implement_strStr/src/com/lingyejun/leetcode/ImplementStrStr.java
@@ -25,7 +25,7 @@ public class ImplementStrStr {
      * Implement strStr()
      *
      * Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
-     * 
+     *
      * @param haystack
      * @param needle
      * @return
